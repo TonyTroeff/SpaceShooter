9d0ee3d45c82335f3333d67314efd6f49051b3d0
Tonyslav Troev <tony13troev@gmail.com>
2019-05-22 15:51:32 +0300

    Implemented the encryption - decryption serialization feature.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,30 @@
 ﻿namespace SpaceShooter
 {
+	using Serialization;
 	using UnityEngine;
+	using Utilities;
 
 	public class PlayerController : MonoBehaviour
 	{
+		private PlayerProgressInfo _playerProgress;
 		private Rigidbody _rigidbody;
 
 		public float Speed = 1;
 		public float Tilt = 1;
 
-		private void Awake() => this._rigidbody = this.GetComponent<Rigidbody>();
+		private void Awake()
+		{
+			this._rigidbody = this.GetComponent<Rigidbody>();
+			this._playerProgress = Serializer.Load<PlayerProgressInfo>() ?? new PlayerProgressInfo();
+		}
 
 		private void FixedUpdate()
 		{
 			float horizontalMovement = Input.GetAxis("Horizontal");
 			float verticalMovement = Input.GetAxis("Vertical");
-			
+
 			this._rigidbody.velocity = new Vector3(horizontalMovement, 0f, verticalMovement) * this.Speed;
-		
+
 			this._rigidbody.position = new Vector3(
 				Mathf.Clamp(
 					this._rigidbody.position.x,
@@ -25,7 +32,7 @@
 					ScreenController.Dimensions.x - 1),
 				0f,
 				Mathf.Clamp(this._rigidbody.position.z, -3.5f, ScreenController.Dimensions.z - 1));
-		
+
 			this._rigidbody.rotation = Quaternion.Euler(0f, 0f, this._rigidbody.velocity.x * this.Tilt * -1);
 		}
 
--- Assets/Scripts/Serialization/Serializer.cs
+++ Assets/Scripts/Serialization/Serializer.cs
@@ -1,6 +1,9 @@
 namespace SpaceShooter.Serialization
 {
+	using System;
 	using System.IO;
+	using System.Security.Cryptography;
+	using System.Text;
 	using UnityEngine;
 
 	public static class Serializer
@@ -11,18 +14,82 @@ namespace SpaceShooter.Serialization
 		public static void Save<T>(T obj) where T : class
 		{
 			string json = JsonUtility.ToJson(obj);
-			
-			File.WriteAllText(Application.persistentDataPath + "PlayerProgress.json", json);
-			// TODO: Encrypt the json;
+
+			byte[] jsonToBytes = Encoding.UTF8.GetBytes(json);
+
+			using (RijndaelManaged rijndael = new RijndaelManaged())
+			{
+				rijndael.BlockSize = 128;
+				rijndael.KeySize = 256;
+
+				byte[] salt = new byte[128];
+				using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider()) rng.GetBytes(salt);
+
+				byte[] key;
+				using (Rfc2898DeriveBytes rfc = new Rfc2898DeriveBytes(_encryptionKey, salt))
+					key = rfc.GetBytes(rijndael.KeySize / 8);
+
+				rijndael.Key = key;
+				rijndael.GenerateIV();
+
+				using (ICryptoTransform encryptor = rijndael.CreateEncryptor())
+				{
+					byte[] encryptedJsonBytes = encryptor.TransformFinalBlock(jsonToBytes, 0, jsonToBytes.Length);
+
+					// 44 symbols long
+					string encryptedJson = Convert.ToBase64String(encryptedJsonBytes);
+
+					// 172 symbols long
+					string saltString = Convert.ToBase64String(salt);
+
+					// 25 symbols long.
+					string ivString = Convert.ToBase64String(rijndael.IV);
+
+					File.WriteAllText(GetPath<T>(), encryptedJson + saltString + ivString);
+				}
+			}
 		}
 
 		public static T Load<T>() where T : class
 		{
-			string json = File.ReadAllText(Application.persistentDataPath + "PlayerProgress.json");
-			// TODO: Decrypt the json;
-			
-			T obj = JsonUtility.FromJson<T>(json);
-			return obj;
+			string path = GetPath<T>();
+
+			if (File.Exists(path) == false) return null;
+
+			string json = File.ReadAllText(path);
+
+			using (RijndaelManaged rijndael = new RijndaelManaged())
+			{
+				rijndael.BlockSize = 128;
+				rijndael.KeySize = 256;
+
+				string encryptedJson = json.Substring(0, 44);
+				byte[] encryptedJsonBytes = Convert.FromBase64String(encryptedJson);
+
+				string saltString = json.Substring(44, 172);
+				byte[] salt = Convert.FromBase64String(saltString);
+
+				string ivString = json.Substring(216);
+				byte[] iv = Convert.FromBase64String(ivString);
+
+				byte[] key;
+				using (Rfc2898DeriveBytes rfc = new Rfc2898DeriveBytes(_encryptionKey, salt))
+					key = rfc.GetBytes(rijndael.KeySize / 8);
+
+				using (ICryptoTransform decryptor = rijndael.CreateDecryptor(key, iv))
+				{
+					byte[] decryptedJsonBytes = decryptor.TransformFinalBlock(
+						encryptedJsonBytes,
+						0,
+						encryptedJsonBytes.Length);
+					string decryptedJson = Encoding.UTF8.GetString(decryptedJsonBytes);
+
+					T obj = JsonUtility.FromJson<T>(decryptedJson);
+					return obj;
+				}
+			}
 		}
+
+		private static string GetPath<T>() => $"{Application.persistentDataPath}/{typeof(T).Name}.plyi";
 	}
 }
